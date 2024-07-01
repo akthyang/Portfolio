2aff197fd603c8060b279a04a66a2400dc3c4795
Alicia Yang <akthyang@gmail.com>
2024-07-01 12:09:44 +0800

    changed location of media, updated staticfiles, added a new project to test

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n_t+hzi0%r)9pzzw2lnei#ts4g^t45d$2cth3_cshyfq9&1y%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -139,4 +139,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "/media/"
 
 # actual location of the files on server
-MEDIA_ROOT = os.path.join(BASE_DIR, "main/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "main/static/media")
